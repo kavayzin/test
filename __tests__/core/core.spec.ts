89923a2feb71015d4c1fcff8eccb6d8ca75c2916
zavdima36 <zavdima36@gmail.com>
2019-12-23 11:59:07 +0500

    update tests

--- __tests__/core/core.spec.ts
+++ __tests__/core/core.spec.ts
@@ -1,39 +1,39 @@
-// import { EmailsEditor } from '../../src';
+import { EmailsEditor } from '../../src';
 import { createElement, IVNode, render } from '../../src/utils';
 
-// function constrictFormOptions() {
-//   const rootEl = document.createElement('div');
-//   return {
-//     rootEl,
-//     buttons: []
-//   };
-// }
+function constrictFormOptions() {
+  const rootEl = document.createElement('div');
+  return {
+    rootEl,
+    buttons: []
+  };
+}
 
-// describe('EmailForm', () => {
-//   it('add emails', () => {
-//     const form = new EmailsEditor(constrictFormOptions());
-//     const emailsString = 'john@miro.com hhh@hh.tu, hhh@hh.tu';
-//     form.addEmail(emailsString);
-//     expect(form.getEmails()).toEqual(['john@miro.com', 'hhh@hh.tu']);
-//   });
+describe('EmailForm', () => {
+  it('add emails', () => {
+    const form = new EmailsEditor(constrictFormOptions());
+    const emailsString = 'john@miro.com hhh@hh.tu, hhh@hh.tu';
+    form.addEmail(emailsString);
+    expect(form.getEmails()).toEqual(['john@miro.com', 'hhh@hh.tu']);
+  });
 
-//   it('add wrong strings', () => {
-//     const form = new EmailsEditor(constrictFormOptions());
-//     const emails = ['john@miro.com', 'qwe'];
-//     const emailsString = emails.join(' ');
-//     form.addEmail(emailsString);
-//     expect(form.getEmails()).toEqual(['john@miro.com']);
-//   });
+  it('add wrong strings', () => {
+    const form = new EmailsEditor(constrictFormOptions());
+    const emails = ['john@miro.com', 'qwe'];
+    const emailsString = emails.join(' ');
+    form.addEmail(emailsString);
+    expect(form.getEmails()).toEqual(['john@miro.com']);
+  });
 
-//   it('remove emails', () => {
-//     const form = new EmailsEditor(constrictFormOptions());
-//     const emails = ['john@miro.com', 'hhh@hh.tu'];
-//     const emailsString = emails.join(' ');
-//     form.addEmail(emailsString);
-//     form.removeEmail('hhh@hh.tu');
-//     expect(form.getEmails()).toEqual(['john@miro.com']);
-//   });
-// });
+  it('remove emails', () => {
+    const form = new EmailsEditor(constrictFormOptions());
+    const emails = ['john@miro.com', 'hhh@hh.tu'];
+    const emailsString = emails.join(' ');
+    form.addEmail(emailsString);
+    form.removeEmail('hhh@hh.tu');
+    expect(form.getEmails()).toEqual(['john@miro.com']);
+  });
+});
 
 describe('Utils.createElement', () => {
   it('simple div', () => {
